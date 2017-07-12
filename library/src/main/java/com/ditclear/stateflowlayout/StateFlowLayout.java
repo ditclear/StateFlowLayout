fdfffb72f6cf2ef609435e200052e2b1cac7bbcc
weiyixing <weiyixing@hddomain.cn>
2017-07-12 15:09:41 +0800

    measure

--- library/src/main/java/com/ditclear/stateflowlayout/StateFlowLayout.java
+++ library/src/main/java/com/ditclear/stateflowlayout/StateFlowLayout.java
@@ -6,15 +6,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.InflateException;
 import android.view.View;
-import android.widget.ImageView;
 
 /**
  * 页面描述：{@link Node}
@@ -59,7 +60,12 @@ public class StateFlowLayout extends LinearLayoutCompat {
         }
 
         setOrientation(HORIZONTAL);
-        setShowDividers(SHOW_DIVIDER_MIDDLE);
+        if (getDividerDrawable() == null) {
+            setDividerDrawable(new ColorDrawable(Color.BLACK));
+            setShowDividers(SHOW_DIVIDER_NONE);
+        } else {
+            setShowDividers(SHOW_DIVIDER_MIDDLE);
+        }
 
         mStatePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mStatePaint.setColor(stateColor);
@@ -89,10 +95,10 @@ public class StateFlowLayout extends LinearLayoutCompat {
 
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
-            if (!(child instanceof Node)) {
-                throw new InflateException(child.getClass().getSimpleName()
-                        + "not implement interface Node");
-            }
+//            if (!(child instanceof Node)) {
+//                throw new InflateException(child.getClass().getSimpleName()
+//                        + "not implement interface Node");
+//            }
         }
 
         setGravity(Gravity.CENTER);
@@ -105,43 +111,66 @@ public class StateFlowLayout extends LinearLayoutCompat {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        if (heightMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight() + rect.height() * 3);
+        int plusWidth = 0;
+        int plusHeight = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child;
+            if (i == 0 || i == getChildCount() - 1) {
+                child = getChildAt(i);
+                if (child instanceof Node) {
+                    Node node = (Node) child;
+                    String state = node.getState();
+                    String content = node.getContent();
+                    mStatePaint.getTextBounds(state, 0, state.length(), rect);
+                    int width = rect.width();
+                    int height = rect.height();
+                    mContentPaint.getTextBounds(content, 0, content.length(), rect);
+                    plusWidth += (width > rect.width() ? width : rect.width()) / 2;
+                    plusHeight += height + rect.height();
+                }
+            }
         }
-        showDividerMiddle = getShowDividers() == SHOW_DIVIDER_MIDDLE;
-        if (getDividerDrawable() != null && showDividerMiddle) {
-            mDividerWidth = getDividerDrawable().getIntrinsicWidth();
 
-        }
+//        if (getDividerDrawable() != null && showDividerMiddle) {
+//            mDividerWidth = getDividerDrawable().getIntrinsicWidth();
+//
+//        }
 
-        if (widthMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight());
+        if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(getMeasuredWidth() + plusWidth,
+                    getMeasuredHeight() + plusHeight);
+        } else if (widthMeasureSpec == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(getMeasuredWidth() + plusWidth,
+                    getMeasuredHeight());
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(getMeasuredWidth(),
+                    getMeasuredHeight() + plusHeight);
         }
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        if (!showDividerMiddle) {
-            return;
-        }
+//        if (!showDividerMiddle) {
+//            return;
+//        }
         int childNodeCount = 0;
-        for (int i = 0; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-            Node node = (Node) child;
-            int childLeft = child.getLeft();
-            int childRight = child.getRight();
-            if (hasDividerBeforeChildAt(i)) {
-                if (node.isChild() && i != 0) {
-                    childNodeCount++;
-                }
-                childLeft -= node.isChild() ? mDividerWidth / 2 * childNodeCount
-                        : mDividerWidth / 2 * (childNodeCount + 1);
-                childRight -= node.isChild() ? mDividerWidth / 2 * childNodeCount
-                        : mDividerWidth / 2 * (childNodeCount + 1);
-                child.layout(childLeft, child.getTop(), childRight, child.getBottom());
-            }
-        }
+//        for (int i = 0; i < getChildCount(); i++) {
+//            View child = getChildAt(i);
+//            Node node = (Node) child;
+//            int childLeft = child.getLeft();
+//            int childRight = child.getRight();
+//            if (hasDividerBeforeChildAt(i)) {
+//                if (node.isChild() && i != 0) {
+//                    childNodeCount++;
+//                }
+//                childLeft -= node.isChild() ? mDividerWidth / 2 * childNodeCount
+//                        : mDividerWidth / 2 * (childNodeCount + 1);
+//                childRight -= node.isChild() ? mDividerWidth / 2 * childNodeCount
+//                        : mDividerWidth / 2 * (childNodeCount + 1);
+//                child.layout(childLeft, child.getTop(), childRight, child.getBottom());
+//            }
+//        }
     }
 
     private int getNodeOffset(Node node, int childCount) {
@@ -156,13 +185,19 @@ public class StateFlowLayout extends LinearLayoutCompat {
     @Override
     protected void onDraw(Canvas canvas) {
         int count = getChildCount();
+        Log.d(TAG, "onDraw: ");
+
 
         for (int i = 0; i < count; i++) {
+
             View child = getChildAt(i);
-            Node node = (Node) child;
-            if (child instanceof ImageView) {
-                String state = (String) child.getTag();
-                String content = (String) child.getContentDescription();
+
+            if (child instanceof Node) {
+                Node node = (Node) child;
+
+                String state = node.getState();
+                String content = node.getContent();
+                Log.d(TAG, "onDraw: " + state + "  " + content);
                 if (state != null) {
                     mStatePaint.getTextBounds(state, 0, state.length(), rect);
                     canvas.drawText(state,
@@ -176,20 +211,20 @@ public class StateFlowLayout extends LinearLayoutCompat {
                             child.getLeft() - (rect.width() - child.getMeasuredWidth()) / 2,
                             child.getTop() * 0.8f, mContentPaint);
                 }
-                if (getDividerDrawable() == null || !showDividerMiddle) {
-                    continue;
-                }
-                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
-                final int layoutDirection = ViewCompat.getLayoutDirection(this);
-                int position = 0;
-                if (layoutDirection == LAYOUT_DIRECTION_RTL) {
-                    position = child.getLeft() - lp.leftMargin - getDividerPadding();
-                } else {
-                    position = child.getRight() + lp.rightMargin;
-                }
-                if (i != getChildCount() - 1) {
-                    drawVerticalDivider(canvas, position, node.isChild(), node.isNextChild());
-                }
+//                if (getDividerDrawable() == null || !showDividerMiddle) {
+//                    continue;
+//                }
+//                final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+//                final int layoutDirection = ViewCompat.getLayoutDirection(this);
+//                int position = 0;
+//                if (layoutDirection == LAYOUT_DIRECTION_RTL) {
+//                    position = child.getLeft() - lp.leftMargin - getDividerPadding();
+//                } else {
+//                    position = child.getRight() + lp.rightMargin;
+//                }
+//                if (i != getChildCount() - 1) {
+//                    drawVerticalDivider(canvas, position, node.isChild(), node.isNextChild());
+//                }
 
             }
 
